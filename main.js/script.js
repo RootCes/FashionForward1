18bef913350d8e5433c71125a2ed6dee120f9594
smartcoder207 <104476206+smartcoder207@users.noreply.github.com>
2022-06-27 12:40:05 -0400

    Some changes made today 6/27/2022.

---
+++ main.js/script.js
@@ -0,0 +1,12 @@
+
+//this set of codes will justify the new footer on the screen
+@media only screen and (min-width: 29.8125rem /*477px*/) {
+    .ft-main {
+        justify-content: space-around;
+    }
+}
+@media only screen and (min-width: 77.5rem /*1240px*/ ) {
+    .ft-main {
+        justify-content: space-evenly;
+    }
+}
