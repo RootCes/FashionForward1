b5e526a3aee62d38dc1f5bb7363878a476074419
smartcoder207 <104476206+smartcoder207@users.noreply.github.com>
2022-07-21 12:20:05 -0400

    Added several HTML and CSS frameworks

--- main.js/script.js
+++ main.js/script.js
@@ -1,17 +1,38 @@
 
-//this set of codes will justify the new footer on the screen
-@media only screen and (min-width: 29.8125rem /*477px*/) {
+/*this set of codes will justify the new footer on the screen
+@media only screen and (min-width: 29.8125rem (some comment was here ==477px (Some Comment)/) {
     .ft-main {
         justify-content: space-around;
     }
 }
-@media only screen and (min-width: 77.5rem /*1240px*/ ) {
+@media only screen and (min-width: 77.5rem this is comment===/1240p ===this was a comment) {) {
     .ft-main {
         justify-content: space-evenly;
     }
-}
+}        */
 
-//this script is for the carousel slide in the home/index page
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+
+
+
+
+
+/*this script is for the carousel slide in the home/index page
 
 const button = document.querySelector("[data-carousel-button]")
 
@@ -20,5 +41,5 @@ buttons.forEach(button => {
         const offset = button.dataset.carouselButton === "next" ? 1 : -1
         const slides = button.closest("[data-carousel]").querySelector("[data-slides]")
     })
-})
+}) */
 
